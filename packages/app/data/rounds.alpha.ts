c825fe48d24b5429c66035ee64f767d6954cbad3
ghostffcode <meremezeukah@gmail.com>
2023-05-15 11:55:06 -0500

    update known rounds to beta rounds

---
+++ packages/app/data/rounds.alpha.ts
@@ -0,0 +1,34 @@
+const rounds = [
+  {
+    id: '0xe575282b376E3c9886779A841A2510F1Dd8C2CE4',
+    title: 'Ethereum Infrastructure Round',
+    description:
+      'The first Gitcoin Program Alpha QF rounds on the Grants Protocol will run from January 17th to January 31st and are only open to a subset of existing grantees. There will be 3 separate matching pools: Web3 Open Source Software, Ethereum Infrastructure, and Climate Solutions, each with its own grant explorer page where you can donate to grants in that round.   The Ethereum Infrastructure round will only include projects that are in support of or directly advancing the Ethereum ecosystem. This includes areas like Core client devs, Tooling providers, Showcasing the power of critical ecosystem ideas, and Developer education.',
+    disable: false,
+    chainId: 1,
+  },
+  // {
+  //   id: '0xdf75054cd67217aEe44b4f9E4eBC651c00330938',
+  //   title: 'UNICEF Round',
+  //   description:
+  //     "Between Dec. 9 - 21, 2022, Gitcoin is facilitating a Quadratic Funding grants round for a select group of 10 impact-focused, innovative projects from around the world—including Nepal, Kenya, Argentina, Brazil, and the Philippines. This round is part of UNICEF's Office of Innovation  wider explorations into blockchain technology as a mechanism to test new resource allocation tools and provide greater efficiencies in public bureaucracies.",
+  //   chainId: 1,
+  // },
+  {
+    id: '0x1b165fE4DA6bC58AB8370DDC763d367D29F50Ef0',
+    title: 'Climate Solutions Round',
+    description:
+      'The first Gitcoin Program Alpha QF rounds on the Grants Protocol will run from January 17th to January 31st and are only open to a subset of existing grantees. There will be 3 separate matching pools: Web3 Open Source Software, Ethereum Infrastructure, and Climate Solutions, each with its own grant explorer page where you can donate to grants in that round.   The Grant must be primarily focused on climate solutions (the group may do other work but the grant proposal should be directly related to climate solutions).  The proposal should explicitly outline how this project will help reduce GHGs or is important core infrastructure for web3 climate solutions.',
+    disable: false,
+    chainId: 1,
+  },
+  {
+    id: '0xEBdb4156203C8B35b7A7C6F320786b98E5aC67C3',
+    title: 'Fantom Gitcoin Grants Round 1',
+    description: 'Currently disabled because of subgraph support issues.',
+    disabled: true,
+    chainId: 250,
+  },
+]
+
+export default rounds
--- packages/app/data/rounds.ts
+++ packages/app/data/rounds.ts
@@ -1,9 +1,33 @@
 const rounds = [
   {
-    id: '0xe575282b376E3c9886779A841A2510F1Dd8C2CE4',
-    title: 'Ethereum Infrastructure Round',
+    id: '0x12BB5bBbFE596dbc489d209299B8302c3300fa40',
+    title: 'Web3 Open Source Software Round',
     description:
-      'The first Gitcoin Program Alpha QF rounds on the Grants Protocol will run from January 17th to January 31st and are only open to a subset of existing grantees. There will be 3 separate matching pools: Web3 Open Source Software, Ethereum Infrastructure, and Climate Solutions, each with its own grant explorer page where you can donate to grants in that round.   The Ethereum Infrastructure round will only include projects that are in support of or directly advancing the Ethereum ecosystem. This includes areas like Core client devs, Tooling providers, Showcasing the power of critical ecosystem ideas, and Developer education.',
+      'Web3 OSS grants are open-source projects primarily focused on developing on top of, or advancing the broader Ethereum and/or Web3 industry.',
+    disable: false,
+    chainId: 1,
+  },
+  {
+    id: '0x9C3B81967EafBA0a451E324417DD4F3F353b997b',
+    title: 'Web3 Social',
+    description:
+      'The main criteria for Web3 social round are that you’re actively doing work that accelerates Web3 social space and the overall decentralized world. This can be anything related to Web3 Social, but we are especially interested in Web3 infrastructure layers, info distribution protocols, data storage, Web3 social dApps, and on-chain games. Mask Network is excited to host the Web3 Social grant round with Gitcoin to help web3 social-related projects flourish using Quadratic Funding.',
+    disable: false,
+    chainId: 1,
+  },
+  {
+    id: '0x64E5b2228eF31437909900B38fC42Dd5E4B83147',
+    title: 'ENS Ecosystem',
+    description:
+      'ENS Ecosystem featured round is meant to support projects building on top of ENS. Any project that is growing or enhancing the ecosystem should apply.',
+    disable: false,
+    chainId: 1,
+  },
+  {
+    id: '0x274554EB289004e15A7679123901B7F070dDa0fa',
+    title: 'ZK Tech Round',
+    description:
+      'ZK Tech grants are in support of, or directly advancing zero-knowledge tools, libraries, community, or protocols.',
     disable: false,
     chainId: 1,
   },
@@ -14,21 +38,21 @@ const rounds = [
   //     "Between Dec. 9 - 21, 2022, Gitcoin is facilitating a Quadratic Funding grants round for a select group of 10 impact-focused, innovative projects from around the world—including Nepal, Kenya, Argentina, Brazil, and the Philippines. This round is part of UNICEF's Office of Innovation  wider explorations into blockchain technology as a mechanism to test new resource allocation tools and provide greater efficiencies in public bureaucracies.",
   //   chainId: 1,
   // },
-  {
-    id: '0x1b165fE4DA6bC58AB8370DDC763d367D29F50Ef0',
-    title: 'Climate Solutions Round',
-    description:
-      'The first Gitcoin Program Alpha QF rounds on the Grants Protocol will run from January 17th to January 31st and are only open to a subset of existing grantees. There will be 3 separate matching pools: Web3 Open Source Software, Ethereum Infrastructure, and Climate Solutions, each with its own grant explorer page where you can donate to grants in that round.   The Grant must be primarily focused on climate solutions (the group may do other work but the grant proposal should be directly related to climate solutions).  The proposal should explicitly outline how this project will help reduce GHGs or is important core infrastructure for web3 climate solutions.',
-    disable: false,
-    chainId: 1,
-  },
-  {
-    id: '0xEBdb4156203C8B35b7A7C6F320786b98E5aC67C3',
-    title: 'Fantom Gitcoin Grants Round 1',
-    description: 'Currently disabled because of subgraph support issues.',
-    disabled: true,
-    chainId: 250,
-  },
+  // {
+  //   id: '0x1b165fE4DA6bC58AB8370DDC763d367D29F50Ef0',
+  //   title: 'Climate Solutions Round',
+  //   description:
+  //     'The first Gitcoin Program Alpha QF rounds on the Grants Protocol will run from January 17th to January 31st and are only open to a subset of existing grantees. There will be 3 separate matching pools: Web3 Open Source Software, Ethereum Infrastructure, and Climate Solutions, each with its own grant explorer page where you can donate to grants in that round.   The Grant must be primarily focused on climate solutions (the group may do other work but the grant proposal should be directly related to climate solutions).  The proposal should explicitly outline how this project will help reduce GHGs or is important core infrastructure for web3 climate solutions.',
+  //   disable: false,
+  //   chainId: 1,
+  // },
+  // {
+  //   id: '0xEBdb4156203C8B35b7A7C6F320786b98E5aC67C3',
+  //   title: 'Fantom Gitcoin Grants Round 1',
+  //   description: 'Currently disabled because of subgraph support issues.',
+  //   disabled: true,
+  //   chainId: 250,
+  // },
 ]
 
 export default rounds
